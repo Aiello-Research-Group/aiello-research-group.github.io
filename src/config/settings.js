1ab68aedab76df13275ebc61b8eb32685ccace63
Aiello Research Group <lynn.ngo@columbia.edu>
2024-11-08 14:31:41 -0500

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Aiello Research Group | Robert N. Butler Columbia Aging Center`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
+	url: `https://aiello-research-group.github.io`, // No trailing slash! https://odyssey-theme.sapling.supply
 	name: `Aiello Research Group`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
